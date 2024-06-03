d6d17e0ee50e697c53d6171d0c9577526e570e41
Usser <Usser@USERPC>
2024-06-03 21:32:24 +0300

    Add project files.

---
+++ Kurohtin.UI/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppLB1.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
